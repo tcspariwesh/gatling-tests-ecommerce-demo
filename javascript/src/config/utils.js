ad59f80e636433e14c5d1df1e6eecf2356e3efeb
karimatwa <karim-atwa@hotmail.com>
2024-07-02 15:49:53 +0200

    remove .env js

--- javascript/src/config/utils.js
+++ javascript/src/config/utils.js
@@ -1,6 +1,6 @@
 import targetEnvResolver from "./targetEnvResolver";
 
-export const TARGET_ENV = process.env.TARGET_ENV;
+export const TARGET_ENV = "DEV1";
 export const users = 1;
 export const duration = 1;
 export const type = "smoke";
