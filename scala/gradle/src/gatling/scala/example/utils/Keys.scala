7e49fc0d858358369a5b15bca1908ef2ec9abd5f
slandelle <slandelle@gatling.io>
2025-03-24 13:19:39 +0100

    chore: remove useless semicolons

--- scala/gradle/src/gatling/scala/example/utils/Keys.scala
+++ scala/gradle/src/gatling/scala/example/utils/Keys.scala
@@ -2,10 +2,10 @@ package example.utils;
 
 object Keys {
 
-  val ACCESS_TOKEN: String = "AccessToken";
-  val SESSION_ID: String = "SessionId";
-  val PAGE_INDEX: String = "PageIndex";
-  val PRODUCTS: String = "Products";
-  val PRODUCT_NAME: String = "ProductName";
-  val CART_ITEMS: String = "CartItems";
+  val ACCESS_TOKEN: String = "AccessToken"
+  val SESSION_ID: String = "SessionId"
+  val PAGE_INDEX: String = "PageIndex"
+  val PRODUCTS: String = "Products"
+  val PRODUCT_NAME: String = "ProductName"
+  val CART_ITEMS: String = "CartItems"
 }
