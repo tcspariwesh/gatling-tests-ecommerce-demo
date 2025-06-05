579d43a0c23e2a169065d3d3f4a955560a1c803c
tcspariwesh <88646867+tcspariwesh@users.noreply.github.com>
2025-06-05 11:56:28 +0530

    Delete typescript directory

--- typescript/src/utils/keys.ts
+++
@@ -1,6 +0,0 @@
-export const ACCESS_TOKEN = "AccessToken";
-export const SESSION_ID = "SessionId";
-export const PAGE_INDEX = "PageIndex";
-export const PRODUCTS = "Products";
-export const PRODUCT_NAME = "ProductName";
-export const CART_ITEMS = "CartItems";
